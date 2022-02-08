c170b28a1732273f554386f31d58e3341bbfe24b
trexa81 <trex81@yandex.ru>
2022-02-08 10:19:48 +0300

    добавил заглушки

--- File Manager/DEL.cs
+++ File Manager/DEL.cs
@@ -1,6 +1,19 @@
-﻿namespace File_Manager
+﻿using System;
+
+namespace File_Manager
 {
     internal class DEL : IManager
     {
+        public string Name => "stab";
+
+        public string Description => "0.Пустое задание";
+        /// <summary>
+        /// заглушка
+        /// </summary>
+        public void Demo()
+        {
+            Console.WriteLine("Пустой");
+
+        }
     }
 }
